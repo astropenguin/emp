2d32847b6aab106ff880cdb517f960905b3d6ce5
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2018-05-05 17:09:45 +0900

    :+1: Add package information

--- emp/__init__.py
+++ emp/__init__.py
@@ -1,5 +1,11 @@
 # coding: utf-8
 
+from __future__ import absolute_import, print_function, unicode_literals
+
+# information
+__version__ = '0.1'
+
+# submodules
 from .api import *
 from .utils import *
 del api
